bf743d3801afe18948bcec6d96679fd75ecae925
thales <thalesmdias@gmail.com>
2025-06-25 22:12:39 -0300

    binary search

--- busca/binary_search.go
+++ busca/binary_search.go
@@ -1,14 +1,19 @@
 package busca
 
+import "fmt"
+
 func Binary_Search(arr [11]int, alvo int) int {
 
 	inicio := 0
 	fim := len(arr) - 1
+	cont := 0
 
 	for inicio <= fim {
+		cont++
 		meio := (inicio + fim) / 2
 
 		if arr[meio] == alvo {
+			fmt.Println("Número de iterações:", cont)
 			return meio // achou!
 		} else if arr[meio] < alvo {
 			inicio = meio + 1
@@ -16,6 +21,8 @@ func Binary_Search(arr [11]int, alvo int) int {
 			fim = meio - 1
 		}
 	}
+
+	fmt.Println("Número de iterações:", cont)
 
 	return -1 // não achou
 }
